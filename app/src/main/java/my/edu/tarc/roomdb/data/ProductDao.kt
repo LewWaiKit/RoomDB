8e61b040388f7067dc77737e1a3adf7a85ff2c32
LewWaiKit <kitwai2013@gmail.com>
2021-08-26 17:20:02 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/roomdb/data/ProductDao.kt
@@ -0,0 +1,22 @@
+package my.edu.tarc.roomdb.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+
+interface ProductDao {
+
+    @Insert
+    fun insertProduct(product:Product)
+
+    @Query("SELECT * FROM product_table")
+    fun getAll() : List<Product>
+
+    @Query("SELECT * FROM product_table WHERE price< :priceRange")
+    fun getPriceBelow(priceRange:Double) : List<Product>
+
+
+}
